fa4689d988ef70aee94f61fa3d930ad73b682afb
mountAP <ap44122@gmail.com>
2021-02-05 13:14:31 +0530

    added main

---
+++ priorityQueue/main.cpp
@@ -0,0 +1,30 @@
+#include "Queue.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  Queue q;
+  q.insert(2);
+  q.insert(3);
+  q.insert(4);
+  q.insert(21);
+  q.insert(34);
+  q.insert(9);
+  q.display();
+
+  cout << " Element " << q.remove() << " is removed" << endl;
+  cout << " Element " << q.remove() << " is removed" << endl;
+  q.display();
+
+  q.clearAll();
+
+  q.display();
+  q.insert(6);
+  q.insert(211);
+  q.insert(334);
+  q.insert(90);
+  q.display();
+
+  return 0;
+}
